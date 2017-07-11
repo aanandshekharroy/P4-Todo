8b03b781e6c5d1a7ffc5d3ad1478e03e0888d6a9
theseus <aanandshekharroy@gmail.com>
2017-07-11 09:40:42 +0530

    Feat:Init project

---
+++ app/src/main/java/com/c3cyberclub/p5_todoapp/DatabaseHandler.java
@@ -0,0 +1,50 @@
+package com.c3cyberclub.p5_todoapp;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by theseus on 10/7/17.
+ */
+public class DatabaseHandler extends SQLiteOpenHelper {
+
+    // All Static variables
+    // Database Version
+    private static final int DATABASE_VERSION = 1;
+
+    // Database Name
+    private static final String DATABASE_NAME = "to_do_database";
+
+    // Contacts table name
+    public static final String TABLE_TODOS = "Todos";
+
+    // Contacts Table Columns names
+    private static final String KEY_ID = "id";
+    public static final String KEY_NAME = "name";
+    public static final String KEY_DEADLINE = "deadline";
+    public static final String KEY_CREATED_AT="created_at";
+
+    public DatabaseHandler(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    // Creating Tables
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String CREATE_CONTACTS_TABLE = "CREATE TABLE " + TABLE_TODOS + "("
+                + KEY_ID + " INTEGER PRIMARY KEY,"
+                + KEY_NAME + " TEXT,"
+                + KEY_DEADLINE + " TEXT,"
+                + KEY_CREATED_AT + " TEXT"
+                + ")";
+        db.execSQL(CREATE_CONTACTS_TABLE);
+    }
+
+    // Upgrading database
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // Drop older table if existed
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_TODOS);
+    }
+}
---
+++ app/src/main/java/com/c3cyberclub/p5_todoapp/NewTask.java
@@ -0,0 +1,25 @@
+package com.c3cyberclub.p5_todoapp;
+
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewTask extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_task);
+        finish();
+    }
+    public void addTodo(String name, String deadline,String created_at){
+        ContentValues cv=new ContentValues();
+        cv.put(DatabaseHandler.KEY_NAME,name);
+        cv.put(DatabaseHandler.KEY_DEADLINE,deadline);
+        cv.put(DatabaseHandler.KEY_CREATED_AT,created_at);
+        SQLiteDatabase db = new DatabaseHandler(this).getWritableDatabase();
+        db.insert(DatabaseHandler.TABLE_TODOS,null,cv);
+        finish();
+    }
+}
